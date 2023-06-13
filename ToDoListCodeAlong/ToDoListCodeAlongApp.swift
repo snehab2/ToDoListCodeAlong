9d2ce8703c3d3412686f55d01e597e5a3931ccd9
Sneha <snehab@prodigy.net>
2023-06-13 10:27:29 -0500

    Initial Commit

---
+++ ToDoListCodeAlong/ToDoListCodeAlongApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListCodeAlongApp.swift
+//  ToDoListCodeAlong
+//
+//  Created by scholar on 6/13/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListCodeAlongApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
